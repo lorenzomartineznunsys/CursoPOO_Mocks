bb21b6331ea702a5a551e444ff37e677c0ef16d6
NUNSYS\lorenzo.martinez <lorenzo.martinez@nunsys.com>
2016-10-10 13:18:09 +0200

    Optimizados los imports del test

--- test/com/nunsys/cursoPOO/classes/PricingServiceImplTest.java
+++ test/com/nunsys/cursoPOO/classes/PricingServiceImplTest.java
@@ -7,14 +7,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.mockito.verification.VerificationMode;
 
 import java.math.BigDecimal;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by lorenzo.martinez on 10/10/2016.
